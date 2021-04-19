5b1f9a21fcf13d536fe423ffad41a8a73c83d2d8
JelleAverink <j.averink2001@gmail.com>
2021-04-19 19:34:56 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,78 +1,75 @@
-let classifier;
+let fotoClassifier;
 let video;
-let currentWord;
-let currentIndex = 0;
-let isPlaying = false;
-const words = ['banana', 'watch', 'shoe', 'book', 'phone', 'keyboard', 'shirt', 'pants', 'cup'];
-// Create a new p5.speech object/word
-const myVoice = new p5.Speech();
-
-
+let woord;
+let mijnIndex = 0;
+let speeltSpel = false;
+const woorden = ['hair spray', 'umbrella', 'shoe', 'book', 'phone', 'lighter', 'wallet', 'comic book', 'mouse'];
+// dit zorgt dat de p5.speech bepaalde woorden kan uitseprekn
+const praten = new p5.Speech();
 
 function setup() {
     noCanvas();
-    // Cam input
+    //webcam laten werken
     video = createCapture(VIDEO);
-    // Initialize the Image Classifier method with MobileNet and the video as the second argument
-    classifier = ml5.imageClassifier('MobileNet', video, modelReady);
+    //Laat de MobileNet/ml5 classiefier werken
+    fotoClassifier = ml5.imageClassifier('MobileNet', video, modelReady);
 
+    //beginnen
     select('#start').mousePressed(function () {
-        playNextWord();
+        VolgendWoord();
     });
 
+    //volgendwoord
     select('#next').mousePressed(function () {
-        currentIndex++;
-        if (currentIndex >= words.length) {
-            currentIndex = 0;
+        mijnIndex++;
+        if (mijnIndex >= woorden.length) {
+            mijnIndex = 0;
         }
-        playNextWord();
+        VolgendWoord();
     });
 
-    myVoice.onEnd = speechEnded;
+    praten.onEnd = stopPraten;
 }
 
-function playNextWord() {
-    isPlaying = true;
-    currentWord = words[currentIndex];
-    select('#instruction').html(`Go find ${currentWord}!`);
-    // start classify the video
-    classifyVideo();
+function VolgendWoord() {
+    speeltSpel = true;
+    woord = woorden[mijnIndex];
+    select('#instruction').html(`Go find ${woord}!`);
+    //begin foto classification
+    fotoClassificationVideo();
 }
 
 function modelReady() {
-    // Change the state
+    // model gaat van wacht ff naar je kan app gebruiken
     select('#status').html('Model Loaded');
 }
 
-// Get a prediction for the current video frame
-function classifyVideo() {
-    classifier.classify(gotResult);
+// krijg een resultaat
+function fotoClassificationVideo() {
+    fotoClassifier.classify(gotResult);
 }
 
-// When we get a result
-function gotResult(err, results) {
-    // The results are in an array ordered by confidence.
-    // Get the first result string
+// eyy je hebt een resultaat
+function gotResult(error, results) {
+    // check 1e resultaat
     const result = results[0].label;
-    // Split the first result string by coma and get the first word
-    const oneWordRes = result.split(',')[0];
-    // Get the top 3 results as strings in an array
-    // Read more about map function here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-    const top3Res = results.map(r => r.label);
-    // Find if any of the top 3 result strings includes the current word
-    // Read more about find function here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-    const ifFound = top3Res.find(r => r.includes(currentWord))
-    if (ifFound) {
-        // If top 3 results includes the current word
-        isPlaying = false;
-        select('#message').html(`You found ${currentWord}!`);
-        myVoice.speak(`You found ${currentWord}!`);
+    // kies 1 woord
+    const oneWoord = result.split(',')[0];
+    //pakt de beste opties
+    const top3 = results.map(r => r.label);
+    // checked of woord goed is
+    const Gevonden = top3.find(r => r.includes(woord))
+    if (Gevonden) {
+        //als je item hebt gevonden game stopt
+        speeltSpel = false;
+        select('#message').html(`You found ${woord}!`);
+        praten.speak(`You found ${woord}!`);
     } else {
-        select('#message').html(`I see ${oneWordRes}`);
-        myVoice.speak(`I see ${oneWordRes}`);
+        select('#message').html(`I see ${oneWoord}`);
+        praten.speak(`I see ${oneWoord}`);
     }
 }
 
-function speechEnded() {
-    if (isPlaying) classifyVideo();
+function stopPraten() {
+    if (speeltSpel) fotoClassificationVideo();
 }
